4a3e4f297d49bbf6b24069274f9888c56feb2367
Sion <sionn908@gmail.com>
2022-12-07 10:15:08 +0900

    change filename to index

--- baby.js
+++ baby.js
@@ -40,7 +40,7 @@ function main() {
         var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", { size: 1000.0 }, scene);
         var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
         skyboxMaterial.backFaceCulling = false;
-        skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("./texs/skybox", scene);
+        skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("./skybox", scene);
         skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
         skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
         skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+echo 'git add .'
 git add .
+echo 'git commit -m "change filename to index"'
 git commit -m "change filename to index"
+echo 'git push -u origin master'
 git push -u origin master
